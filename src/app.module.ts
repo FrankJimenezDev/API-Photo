234dece00d3886d4f03bee4dec5ae03bf191ed9e
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2025-09-23 09:58:11 -0300

    [Config] Configuraca coneccion a DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhotoModule } from './photo/photo.module';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [PhotoModule],
+  imports: [
+    PhotoModule,
+    DbModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Esto hace que ConfigService sea global
+      envFilePath: '.env.development.local', // opcional, si querés cargar tu .env
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db/db.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<'mysql'>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DbModule {}
