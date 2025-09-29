6a9866bd099f92009bd6de12be08714efc7a112a
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2025-09-29 00:53:49 -0300

    [Config] Se inicializa y configura transactionalContext con AppDataSource de datasource.config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import "reflect-metadata"
+import { initializeTransactionalContext, addTransactionalDataSource  } from 'typeorm-transactional';
+import { AppDataSource } from './db/config/datasource.config';
 
 async function bootstrap() {
+   initializeTransactionalContext();
+
+  await AppDataSource.initialize();
+  addTransactionalDataSource(AppDataSource);
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
+
